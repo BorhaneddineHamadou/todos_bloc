867fd0b981c6b8e782589c7d9f1e8a5deff6f46b
borhaneddine <haborhaneddine@gmail.com>
2024-05-06 13:28:25 +0100

    adding refresh indicator to todos

--- lib/widgets/todos_widget.dart
+++ lib/widgets/todos_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todosproject/bloc/todos_bloc.dart';
 
 import '../models/todo.dart';
 
@@ -9,23 +11,28 @@ class TodosWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return Padding(
-              padding: const EdgeInsets.all(8),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  child: Text(todos[index].id.toString()),
+    return RefreshIndicator(
+        child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    child: Text(todos[index].id.toString()),
+                  ),
+                  title: Text(todos[index].title),
+                  trailing: todos[index].completed
+                      ? const Icon(Icons.done, color: Colors.green)
+                      : null,
                 ),
-                title: Text(todos[index].title),
-                trailing: todos[index].completed
-                    ? const Icon(Icons.done, color: Colors.green)
-                    : null,
-              ),
-          );
-        }
+              );
+            }
+        ),
+        onRefresh: () async{
+          context.read<TodosBloc>().add(GetAllTodosEvent());
+        },
     );
   }
 
